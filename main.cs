4c7c24e8c22d436e636ae5fe7a2176c77cd3af33
jonaerollins <jonaerollins@gmail.com>
2021-02-04 04:40:56 +0000

    Added code

--- main.cs
+++ main.cs
@@ -2,6 +2,26 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+
+ Console.WriteLine("Lab2a");
+ 
+ int apple = 10;
+ 
+ Console.WriteLine("Enter the name of item");
+ Console.ReadLine(); 
+ 
+ Console.WriteLine("Enter the quantity");
+ int quantity = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Enter the price");
+ Console.ReadLine();
+
+ Console.WriteLine("Total price of apple+= $7.5");
+ 
+
+ 
+
+ 
+ 
+ }
 }
